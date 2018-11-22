7844fb46b714444d763492956e9c0f103c6bfebf
Phakjira <phakjra2000@gmail.com>
2018-11-22 23:21:10 +0700

    added a mock splash screen

--- NamJai/app/src/main/java/com/example/phakjira/namjai/MainActivity.java
+++ NamJai/app/src/main/java/com/example/phakjira/namjai/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.phakjira.namjai;
 
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    private TextView NamJai;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NamJai = (TextView) findViewById(R.id.nam);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        NamJai.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent welcomeIntent = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(welcomeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
